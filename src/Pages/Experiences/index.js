506611c563de7264ca342cd20310bb0c238d47bd
Kevin Arellano <Kevin.Arellano94@gmail.com>
2021-08-01 23:46:48 -0400

    Experience CSS adjustment
    Adjusted JSON data coming in to be responsive in mobile.

--- src/Pages/Experiences/index.js
+++ src/Pages/Experiences/index.js
@@ -4,21 +4,32 @@ data.sort(( a, b ) => {
     return b.id - a.id;
 });
 
+var i = 0;
+
 function Experiences() {
     return (
         <div className = "content">
-            <div className = "main-text">
+            <div className = "experience-text">
                 <h1>Experience</h1>
-              {
-                data.map((post, index) =>
-                    <div key={index}>
+                {
+                    data.map( ( post, key ) =>
+                    <div key = { key }>
                         <h2>{ post.position }</h2>
                         <h4>{ post.company }</h4>
                         <p>{ post.date }</p>
-                        <p>{ post.description }</p>
+                        <p>{ post.description[i].a }</p>
+                        <p>{ post.description[i].b }</p>
+                        <p>{ post.description[i].c }</p>
+                        <p>{ post.description[i].d }</p>
+                        <p>{ post.description[i].e }</p>
+                        <p>{ post.description[i].f }</p>
+                        <p>{ post.description[i].g }</p>
+                        <p>{ post.description[i].h }</p>
+                        <p>{ post.description[i].i }</p>
+                        <p>{ post.description[i].j }</p>
                     </div>
                     )
-              }
+                }
             </div>
         </div>
     );
