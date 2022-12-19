88a87c3acef31a62e1f21941bbe956dfac9bc701
KevinArellano94 <Kevin.Arellano94@GMail.Com>
2022-12-18 20:32:46 -0500

    Updated statusCode variable name.

--- functions/supermario.js
+++ functions/supermario.js
@@ -4,7 +4,7 @@ exports.handler = async () => {
   const data = { name: 'mario', age: 35, job: 'plumber' }
 
   return {
-    status: 200,
+    statusCode: 200,
     body: JSON.stringify(data)
   }
 }
